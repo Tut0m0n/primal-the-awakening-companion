7d18f6a76d2514a4049f32f470a4cf7ba05e129c
Tut0m0n <157429849+Tut0m0n@users.noreply.github.com>
2026-02-04 17:01:32 -0300

    characters.js

--- js/characters.js
+++ js/characters.js
@@ -1,1 +1,18 @@
 // Manejo de personajes
+let character = {
+  name: "Héroe",
+  hp: 20
+};
+
+function takeDamage(amount) {
+  character.hp -= amount;
+  localStorage.setItem("hp", character.hp);
+  updateUI();
+}
+
+function loadCharacter() {
+  const savedHP = localStorage.getItem("hp");
+  if (savedHP !== null) {
+    character.hp = parseInt(savedHP);
+  }
+}
